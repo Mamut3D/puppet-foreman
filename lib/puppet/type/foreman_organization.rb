74ad92bc8922591dbdae214749d99ee63836c97f
Radim Janča <janca@ics.muni.cz>
2018-04-05 16:11:36 +0200

    upd foreman organization puppet resource

--- lib/puppet/type/foreman_organization.rb
+++ lib/puppet/type/foreman_organization.rb
@@ -7,4 +7,40 @@ Puppet::Type.newtype(:foreman_organization) do
     desc 'The name of the organization.'
     isrequired
   end
+
+  newparam(:base_url) do
+    desc 'Foreman\'s base url.'
+  end
+
+  newparam(:effective_user) do
+    desc 'Foreman\'s effective user for the registration (usually admin).'
+  end
+
+  newparam(:consumer_key) do
+    desc 'Foreman oauth consumer_key'
+  end
+
+  newparam(:consumer_secret) do
+    desc 'Foreman oauth consumer_secret'
+  end
+
+  newparam(:ssl_ca) do
+    desc 'Foreman SSL CA (certificate authority) for verification'
+  end
+
+  newparam(:timeout) do
+    desc "Timeout for HTTP(s) requests"
+
+    munge do |value|
+      value = value.shift if value.is_a?(Array)
+      begin
+        value = Integer(value)
+      rescue ArgumentError
+        raise ArgumentError, "The timeout must be a number.", $!.backtrace
+      end
+      [value, 0].max
+    end
+
+    defaultto 500
+  end
 end
